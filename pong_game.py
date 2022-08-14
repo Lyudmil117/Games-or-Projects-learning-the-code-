d85b1399cef972f704b150e12fe14f8c5529be6c
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-08-14 16:50:29 +0300

    Add files via upload

---
+++ pong_game.py
@@ -0,0 +1,136 @@
+import turtle
+import winsound
+
+window = turtle.Screen()
+window.title("Pong by the Beginner")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# Score
+
+score_a = 0
+score_b = 0
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# the ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.06
+ball.dy = 0.06
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Tati: 0    Liq: 0", align="center", font=("Courier", 24, "normal"))
+
+# functions
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+
+# keyboard binding
+
+window.listen()
+window.onkeypress(paddle_a_up, "w")
+window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_b_up, "Up")
+window.onkeypress(paddle_b_down, "Down")
+
+
+
+# main game loop
+while score_a < 10 and score_b < 10:
+    window.update()
+
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("boing-6222.mp3", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("boing-6222.mp3", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        winsound.PlaySound("UKWVN3D-male-posh-haha-1.mp3", winsound.SND_ASYNC)
+        pen.write("Tati: {}    Liq: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        winsound.PlaySound("UKWVN3D-male-posh-haha-1.mp3", winsound.SND_ASYNC)
+        pen.write("Tati: {}    Liq: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+
+    # paddle and ball bounce
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("boing-6222.mp3", winsound.SND_ASYNC)
+
+    if (ball.xcor() < - 340 and ball.xcor() > -350) and (ball.ycor() > paddle_a.ycor() - 40 and ball.ycor() < paddle_a.ycor() + 50):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("boing-6222.mp3", winsound.SND_ASYNC)
+
